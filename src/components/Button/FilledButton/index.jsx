1b14e51954a91b92d5c06a3f55a6123364134929
Jesemeil <otahaghogho@gmail.com>
2024-06-14 16:36:25 +0100

    Modified form

--- src/components/Button/FilledButton/index.jsx
+++ src/components/Button/FilledButton/index.jsx
@@ -1,19 +1,17 @@
-import style from './index.module.css'
+import style from "./index.module.css";
 
-export function FilledButton({text, color, padding, borderRadius}){
-
-  return(
-    <button 
+export function FilledButton({ text, color, padding, borderRadius }) {
+  return (
+    <button
       className={style.filledButton}
       style={{
-          color: color,
-          padding: padding,
-          fontWeight: "bold",
-          borderRadius: borderRadius,
-
-  
-      }}>
-        {text}
+        color: color,
+        padding: padding,
+        fontWeight: "bold",
+        borderRadius: borderRadius,
+      }}
+    >
+      {text}
     </button>
-  )
+  );
 }
